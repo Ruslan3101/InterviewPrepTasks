94d3d9a70040f8a168687ef7cc93473a0603d58f
Rusalan Lutfullin <ruslanlutfullin95@gmail.com>
2024-07-21 12:13:29 -0700

    refactor: add more tasks to react

--- src/rootes/tasks.tsx
+++ src/rootes/tasks.tsx
@@ -15,11 +15,35 @@ const Tasks: React.FC = () => {
       "Array Methods",
     ],
     react: [
-      "Counter",
-      "Axios",
-      "TodoList",
-      "Weather",
-      "Carousel",
+      //Beginner Level
+      "Counter",// Create counter that has to increase and decrease by one
+      "Timer", //Create a timer that counts down from a specified time.
+      "TodoList",//Develop a to-do list application with features to add and remove tasks.
+      "Dynamic Background",//Create a component that changes its background color when clicked.
+      "Data Fetch Axios API", ///Build a component that fetches and displays data from an external API
+      "External Data Fetch JSON",//Build a component that fetches and displays data from an external JSON file.
+      "Routing Setup", //Set up a basic routing system using React Router.
+      "Random Quote Generation", //Build a component that displays a random quote each time it's rendered.
+      "File Uploader",//Develop a file uploader component that allows users to upload images.
+      "Authentication Form", //Implement a basic authentication form with login and registration.
+      "Weather App",//Build a weather app that fetches and displays weather information based on the user's location.
+      //Intermediate Level
+      "Search Bar", //Create a search bar that filters a list of items based on user input.
+      "Real Time Chat App", //App: Build a chat application with real-time messaging using WebSockets.
+      "Drag and Drop", //Implement drag-and-drop functionality to reorder a list of items.
+      "Image Carousel", //Create a carousel component that displays multiple images in a slideshow.
+      "Multi Step Form", //Develop a multi-step form with progress indicators.
+      "Pagination", //Build a pagination component to navigate through a large list of items.
+      "Color Picker", //Implement a color picker that allows users to select a color.
+      "Responsive Navigation Menu",//Create a responsive navigation menu with a hamburger icon.
+      "User Profile Page", //Develop a user profile page that displays user information and allows editing.
+      "Text Search and Highlight",//Implement a search and highlight feature for a long text document.
+      "File Download",//Create a file download component that allows users to download files.
+      "Quiz App",//Build a quiz application with a timer and scoring system.
+      "Kanban Board", // Develop a task management application with features like adding, editing, and deleting tasks.
+      
+    
+      "Accordion",
       "Shopping Cart",
       "LocalStorage Hook",
       "Context API",
